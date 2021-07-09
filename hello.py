5fb75c0dfb4368797fc066b5fdbe5cf1f40df420
Rik Smith <riksmith1@hotmail.com>
2021-07-09 21:33:44 +0100

    initial commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/user/<string:name>')
+def user(name):
+	return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
